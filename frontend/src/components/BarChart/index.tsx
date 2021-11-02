04bfea781cc11fad4ed38676bce6b2681f0a396d
oTalDoHud <hudson.lucas.vieira@gmail.com>
2021-11-02 20:38:56 -0300

    Static Charts/ Graficos estaticos

---
+++ frontend/src/components/BarChart/index.tsx
@@ -0,0 +1,35 @@
+import Chart from "react-apexcharts"
+
+function BarChard() {
+
+    const options = {
+        plotOptions: {
+            bar: {
+                horizontal: true,
+            }
+        },
+    };
+
+    const mockData = {
+        labels: {
+            categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+        },
+        series: [
+            {
+                name: "% Sucesso",
+                data: [43.6, 67.1, 67.7, 45.6, 71.1]
+            }
+        ]
+    };
+
+    return (
+        <Chart
+            options={{ ...options, xaxis: mockData.labels }}
+            series={mockData.series}
+            type="bar"
+            height="250px"
+        />
+    );
+}
+
+export default BarChard;
