9c5036c5123d22d32451ed2646edf3a5fbcc1508
oTalDoHud <hudson.lucas.vieira@gmail.com>
2021-11-01 21:31:43 -0300

    Project created

---
+++ backend/src/test/java/com/HudLuca/DsVendas/DsVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HudLuca.DsVendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
