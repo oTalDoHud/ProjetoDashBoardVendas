4650ad5e857af5757c1228959f9167df434b3fff
oTalDoHud <hudson.lucas.vieira@gmail.com>
2021-11-03 08:58:02 -0300

    Security config/ configuração de segurança

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ function Footer() {
             <div className="container">
                 <p className="text-light">App desenvolvido por <a href="//www.linkedin.com/in/oTalDoHud" target="_blank" rel="noreferrer">Hudson Lucas</a></p>
                 <p className="text-light"><small><strong>Semana Spring React</strong><br />
-                    Agradecimentos especiais a escola DevSuperior (Prof: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">Nélio Alves)</a></small></p>
+                    Agradecimento especial à escola DevSuperior (Prof: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">Nélio Alves)</a></small></p>
             </div>
         </footer>
     );
--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -5,7 +5,7 @@ function NavBar() {
         <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
             <div className="container">
                 <nav className="my-2 my-md-0 mr-md-3">
-                    <a href="https://dsvendas-hudlucas.netlify.app"><img src={hudlucalogo} alt="HudLUCA" width="150" /></a>
+                    <a href="https://dsvendas-hudlucas.netlify.app"><img src={hudlucalogo} alt="HudLUCA" width="170" /></a>
                 </nav>
             </div>
         </div>
