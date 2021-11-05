406f31189f9e2ad515606f6d95ee8770588d7058
oTalDoHud <hudson.lucas.vieira@gmail.com>
2021-11-05 10:56:45 -0300

    Home, navigation

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,11 +1,14 @@
 import hudlucalogo from 'assets/img/hudlucalogo.svg';
+import { Link } from "react-router-dom";
 
 function NavBar() {
     return (
         <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
             <div className="container">
                 <nav className="my-2 my-md-0 mr-md-3">
-                    <a href="https://dsvendas-hudlucas.netlify.app"><img src={hudlucalogo} alt="HudLUCA" width="170" /></a>
+                    <Link to="/">
+                   <img src={hudlucalogo} alt="HudLUCA" width="170" />
+                   </Link>
                 </nav>
             </div>
         </div>
