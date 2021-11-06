837d6e8419dacc5c468abefb35f84c61e4f237a8
oTalDoHud <hudson.lucas.vieira@gmail.com>
2021-11-05 22:51:39 -0300

    BarChart integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,5 +1,10 @@
 export type saleSum =  {
-    sellerName: string,
-    sum: number
+    sellerName: string;
+    sum: number;
 }
 
+export type SaleSuccess = {
+    sellerName: string;
+    visited: number;
+    deals: number;
+}
